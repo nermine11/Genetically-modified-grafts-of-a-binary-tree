3fabe0df6b101138c15df942d8fa357d75ef6f34
abldd <80428118+abldd@users.noreply.github.com>
2024-02-20 10:12:31 +0100

    Update arbres_binaires.h

--- arbres_binaires.h
+++ arbres_binaires.h
@@ -1,3 +1,5 @@
 Arbre cree_A_1(void);
-Arbre cree_A_2(void)
-Arbre cree_A_3(void)
+Arbre cree_A_2(void);
+Arbre cree_A_3(void);
+
+Arbre construire_arbre_binaire(char * mot , const char separateur[3]);
