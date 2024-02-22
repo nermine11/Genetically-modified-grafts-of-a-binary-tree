6573a47e0c00e30d44bafce97d50324fd9d73380
Your Name <narminelkilani@gmail.com>
2024-02-22 17:57:39 +0100

    cree_arbre_A2

--- greffe.c
+++ greffe.c
@@ -3,3 +3,27 @@
 #include <string.h>
 #include "greffe.h"
 #include "arbres_binares.h"
+
+
+
+
+
+
+static greffe(Arbre n, Arbre g){
+    if(!n || !g)
+        return;
+    if(!(n->fg) && !(n->fd))
+        return;
+    if(!(g->fg)) // && n->fd?
+        copie(&(g->fg), n->fd);
+    if(!(g->fd))
+        copie(&(g->fd), n->fd);
+    greffe(n, g->fg);
+    greffe(n, g->fd);
+}
+
+static void expansion_aux(Arbre *n, Arbre b){
+    copie(&g, b);
+    greffe(*n, g);
+    *n = g;
+}
