5d0b8bdea488ccc47310b8f43d924d154159471c
abldd <80428118+abldd@users.noreply.github.com>
2024-02-22 12:22:47 +0100

    Update arbres_binaires.h

--- arbres_binaires.h
+++ arbres_binaires.h
@@ -1,9 +1,12 @@
 #ifndef ARBRES_BINAIRES_H
 #define ARBRES_BINAIRES_H
-
-#define  MAX_MOT 100
-#define NOEUD_EXIST(tokens) (tokens && (strcmp(tokens , "1") == 0)? 1: 0 )
-#define NOEUD_VIDE(tokens) (tokens && (strcmp(tokens , "0") == 0)? 1: 0 )
+#define MAX_MOT 200
+#define NOEUD_VIDE(mot) (((mot == '0'))? 1: 0 )
+#define NOEUD_EXIST(mot) (((mot == '1'))? 1: 0 )
+#define NOEUD_VIDE_STRING(tokens) (((strcmp(tokens , "0") == 0))? 1: 0 )
+#define NOEUD_EXIST_STRING(tokens) (((strcmp(tokens , "1") == 0))? 1: 0 )
+#define MOT_VIDE(tokens) ((strcmp(tokens , " ") == 0)? 1: 0 )
+#define TEST_MOT(saisir , codage)((strcmp(saisir , codage) == 0)? 1: 0 )
 
 typedef struct _noeud Noeud, *Arbre;
 
