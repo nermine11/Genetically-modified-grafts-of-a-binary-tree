69e5cbe0b4f913b773f2149f8d3c916c8c27fe64
Your Name <narminelkilani@gmail.com>
2024-02-26 06:54:46 +0100

    copie

--- greffe.c
+++ greffe.c
@@ -5,13 +5,31 @@
 #include "arbres_binares.h"
 
 
-
-
+int copie(Arbre * dest, Arbre source){
+    if (!source)
+        return 1;
+    if(!(*dest)){
+        *dest = alloue_noeud(source->val);
+        if (!(*dest)){
+            fprintf(stderr, "erreur allocation *dest dans copie()");
+            return 0;
+        }
+    }
+    else
+        strcpy((*dest)->val, source->val);
+    int cg = copie(&((*dest)->fg), source->fg);
+    int cd = copie(&((*dest)->fd), source->fd);
+    if (!cg || !cd) {
+        fprintf(stderr, "erreur dans copie()");
+        return 0;
+    }
+    return 1;
+}
 
 // greffe de n sur g
 // renvoie 0 en cas d'erreur sinon 1
 static int greffe(Arbre *n, Arbre b){
-    Arbre g;
+    Arbre g = NULL;
     if (!(copie(&g, b))){
         fprintf(stderr, "erreur dans copie dans greffe()");
         return 0;
@@ -38,20 +56,15 @@ static int greffe(Arbre *n, Arbre b){
         }
         return 1;
     }
-    greffe(n, g->fg);
-    greffe(n, g->fd);
-    *n = g;
+    int gr_g = greffe(n, g->fg);
+    int gr_d = greffe(n, g->fd);
+    if( !gr_g || !gr_d ) // pas d'erreur dans les greffes
+        return 0;
+    
+    *n = g; 
     return 1;
 } 
 
-int expansion(Arbre * a, Arbre b){
-
-    if (!(*a) || !b){
-        fprintf(stderr, "*a ou b sont vides dans expansion()");
-        return 0;
-    }   
-
-}
 
 
 
