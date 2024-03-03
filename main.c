2118441a44c5eafa5afb8ed36df6e3fd17bf51e6
abldd <80428118+abldd@users.noreply.github.com>
2024-03-03 19:04:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,11 @@ void ecrireDebut(FILE *f){
 
 void ecrireArbre(FILE *f, Arbre a){
     if (a){
-        fprintf(f,"\tn%p [label=\"<gauche> | <valeur> %s | <droit>\"];\n", a, a->s);
+        fprintf(f,"\tn%p [label=\"<gauche> | <valeur> %s | <droit>\"];\n",(void *) a, a->val);
     if (a->fg)
-        fprintf(f, "\tn%p:gauche:c -> n%p:valeur;\n", a, a->fg);
+        fprintf(f, "\tn%p:gauche:c -> n%p:valeur;\n", (void *)a, (void *)a->fg);
     if (a->fd)
-        fprintf(f, "\tn%p:droit:c -> n%p:valeur;\n", a, a->fd);
+        fprintf(f, "\tn%p:droit:c -> n%p:valeur;\n", (void *)a, (void *)a->fd);
 
     ecrireArbre(f, a->fg);
     ecrireArbre(f, a->fd);
@@ -35,6 +35,19 @@ void dessine_arbre(FILE *out, Arbre a){
     ecrireArbre(out, a);
     ecrireFin(out);
 }
+
+void creePDF ( char * dot , char * pdf , Arbre a ) {
+    FILE * out = fopen ( dot , "w ") ;
+    dessine_arbre(out , a ) ;
+    fclose(out) ;
+    int len = strlen (dot) + strlen ( pdf ) + 15 ;
+    char cmd [ len ];
+    strcpy (cmd , "dot -Tpdf " ) ;
+    strcat (cmd , dot );
+    strcat (cmd , " -o ") ;
+    strcat (cmd , pdf ) ;
+    system (cmd) ;
+ }
 
 int main(){
     FILE * out = fopen("Arbre.dot" , "a");
