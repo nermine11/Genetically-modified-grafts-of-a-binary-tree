ecb402997c9933d861a177232305cd85071f2e3c
abldd <80428118+abldd@users.noreply.github.com>
2024-03-10 15:39:34 +0100

    Update arbres_binaires.h

--- include/arbres_binaires.h
+++ include/arbres_binaires.h
@@ -1,5 +1,15 @@
 #ifndef ARBRES_BINAIRES_H
 #define ARBRES_BINAIRES_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_MOT 200
+#define NOEUD_VIDE(mot) (((mot == '0'))? 1: 0 )
+#define NOEUD_EXIST(mot) (((mot == '1'))? 1: 0 )
+#define MOT_VIDE(tokens) ((strcmp(tokens , " ") == 0)? 1: 0 )
+#define NOEUD_STRING_VIDE(mot)  ((strcmp(mot , " NULL") == 0)? 1: 0 )
+#define FORMAT(code , format) ((strstr(code , format ) )? 1 : 0)
+#define MOT_IDENTIQUE(mot , tokens)((strcmp(tokens , mot) == 0)? 1: 0 )
 
 
 typedef struct _noeud {
@@ -18,6 +28,12 @@ void liberer(Arbre * A);
 Arbre cree_A_1(void);
 Arbre cree_A_2(void);
 Arbre cree_A_3(void);
-
-
+int  Copier_Chiffre(char *mot , Memo * p);
+int lire(char * chaine , int longeur  , Memo * p);
+int verification_mot(char * mot);
+void vider_memo(char * code , int taille);
+int creation_code_adapter_arbre(Memo * p);
+int creation_Memo(Memo *p);
+Arbre construire_arbre_binaire(char * mot);
+int construit_arbre(Arbre *a);
 #endif
