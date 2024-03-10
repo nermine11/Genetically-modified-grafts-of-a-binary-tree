7e3c6045e5b31f5f1195ea6595cf379eeea48e79
abldd <80428118+abldd@users.noreply.github.com>
2024-03-10 15:40:21 +0100

    Update saage.h

--- include/saage.h
+++ include/saage.h
@@ -1,24 +1,16 @@
 #ifndef SAAGE_H
 #define SAAGE_H
 #include "greffe.h"
-#define MAX_MOT 200
-#define NOEUD_VIDE(mot) (((mot == '0'))? 1: 0 )
-#define NOEUD_EXIST(mot) (((mot == '1'))? 1: 0 )
-#define MOT_VIDE(tokens) ((strcmp(tokens , " ") == 0)? 1: 0 )
-#define NOEUD_STRING_VIDE(mot)  ((strcmp(mot , " NULL") == 0)? 1: 0 )
-#define FORMAT(code , format) ((strstr(code , format ) )? 1 : 0)
-#define MOT_IDENTIQUE(mot , tokens)((strcmp(tokens , mot) == 0)? 1: 0 )
 
 
 
 
-int  Copier_Chiffre(char *mot , Memo * p , int i);
-int verification_mot(char * mot);
-int creation_Memo(Memo *p);
-int creation_code_adapter_arbre(Memo * p);
-Arbre construire_arbre_binaire(char * mot);
+
+int reallouer(Memo * p);
 void affiche(Arbre A , FILE * out, int nombre_espace );
-int test_format(char * nom_de_fichier );
+char * test_format(char * nom_de_fichier , int modifier, Memo *p);
 int serialise(char * nom_de_fichier, Arbre A);
 int deserialise(char * nom_de_fichier, Arbre * A);
+void nom(char * fichier , char mot[MAX_MOT]);
+
 #endif
