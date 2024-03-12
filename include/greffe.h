3a68dd94fb929aff0c34094e653aafa97a454bf7
abldd <80428118+abldd@users.noreply.github.com>
2024-03-12 17:59:52 +0100

    Update greffe.h

--- include/greffe.h
+++ include/greffe.h
@@ -4,8 +4,16 @@
 #include "arbres_binaires.h"
 
 
+
 int copie(Arbre * dest, Arbre source);
+/*on copie dans *dest l’arbre source On retourne 0 si l’allocation échoue, sinon on retourne 1.*/
 int expansion(Arbre * a, Arbre b);
+/*qui modifie l’arbre *A de sorte qu’après l’appel de fonctions,*/ 
+/*celui-ci contienne la greffe de l’arbre B sur A. */
+/*Celle-ci renverra 1 en cas de succès et 0 en cas d’échec.*/
+
+
+
 
 
 
