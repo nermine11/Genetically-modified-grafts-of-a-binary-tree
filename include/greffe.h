fc2ce297d675c55feafe4002b3cb80a508ece89f
Your Name <narminelkilani@gmail.com>
2024-03-12 20:09:13 +0100

    comments

--- include/greffe.h
+++ include/greffe.h
@@ -4,13 +4,12 @@
 #include "arbres_binaires.h"
 
 
-
+//on copie dans *dest l’arbre source On retourne 0 si l’allocation échoue, sinon on retourne 1.
 int copie(Arbre * dest, Arbre source);
-/*on copie dans *dest l’arbre source On retourne 0 si l’allocation échoue, sinon on retourne 1.*/
+
 int expansion(Arbre * a, Arbre b);
-/*qui modifie l’arbre *A de sorte qu’après l’appel de fonctions,*/ 
-/*celui-ci contienne la greffe de l’arbre B sur A. */
-/*Celle-ci renverra 1 en cas de succès et 0 en cas d’échec.*/
+//on  greffe l'arbre b sur l'arbre a
+// la fonction renvoie 1 en cas de succès et 0 en cas d’échec d'une des fonctions intermediaires
 
 
 
