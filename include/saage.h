072a4560a3f3002fa347fe3ed0fe49ade3b43ff7
abldd <80428118+abldd@users.noreply.github.com>
2024-03-12 18:11:13 +0100

    Update saage.h

--- include/saage.h
+++ include/saage.h
@@ -6,11 +6,15 @@
 
 
 
-int reallouer(Memo * p);
-void affiche(Arbre A , FILE * out, int nombre_espace );
-char * test_format(char * nom_de_fichier , int modifier, Memo *p);
-int serialise(char * nom_de_fichier, Arbre A);
-int deserialise(char * nom_de_fichier, Arbre * A);
-void nom(char * fichier , char mot[MAX_MOT]);
 
+void affiche(Arbre A , FILE * out, int nombre_espace );
+/* écrit d’abord dans le fichier  le sous-arbre gauche ensuite le sous-arbre droit en respectant les conventions données*/
+char * test_format(char * nom_de_fichier , int modifier, Memo *p);
+/*teste si le format (qu'il contient .saage) du fichier est correct il renvoie 1 sinon 0.*/
+int serialise(char * nom_de_fichier, Arbre A);
+/*sauvegarde un arbre dans un fichier en cas de succès on renverra 1 sinon 0.*/
+int deserialise(char * nom_de_fichier, Arbre * A);
+/*créer  un arbre à partir d'un fichier en cas de succès on renverra 1 sinon 0.*/
+void nom(char * fichier , char mot[MAX_MOT]);
+/*retire le format .saage*/
 #endif
